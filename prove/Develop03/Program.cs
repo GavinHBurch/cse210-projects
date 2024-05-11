cd6239a92628f4a8dd716315342bc08a50a5d3f8
GavinHBurch <burchyboy13@gmail.com>
2024-05-10 23:01:13 -0600

    Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
-class Program
+namespace ScriptureMemorizer
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        
+        static void Main()
+        {
+            Scripture scripture = new Scripture("2 Nephi ", 3, 8, "And I will give unto him a commandment that he shall do anone other work, save the work which I shall command him. And I will make him great in mine eyes; for he shall do my work. ");
+            //
+            Console.Clear();
+
+            scripture.Display(); 
+
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to finish:");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWords())
+                {
+                    Console.WriteLine("\nThanks for remembering my favorite scripture");
+                    break;
+                }
+
+                Console.Clear();
+
+                scripture.Display();
+
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to finish:");
+            }
+        }
     }
 }
